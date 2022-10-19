b84ca681ee9ec2249fd6c8e634fe5f52082ce111
Hexadecimally (JU) <joel@joelurw.in>
2022-10-19 02:30:35 +0100

    Initial Commit

--- src/in/joelurw/Lored/ItemManagement.java
+++ src/in/joelurw/Lored/ItemManagement.java
@@ -1,11 +1,8 @@
 package in.joelurw.Lored;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.RecipeChoice;
-import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
--- src/in/joelurw/Lored/commands/Lore.java
+++ src/in/joelurw/Lored/commands/Lore.java
@@ -8,7 +8,6 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
