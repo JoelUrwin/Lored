a18010a924d18be48c19a2df1ddf5a1a75794a7c
Hexadecimally (JU) <joel@joelurw.in>
2022-10-19 02:12:54 +0100

    Initial Commit

--- src/in/joelurw/Lored/Lored.java
+++ src/in/joelurw/Lored/Lored.java
@@ -3,9 +3,6 @@ package in.joelurw.Lored;
 import in.joelurw.Lored.commands.Lore;
 import in.joelurw.Lored.events.LoreScrollApplyEvent;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Lored extends JavaPlugin {
