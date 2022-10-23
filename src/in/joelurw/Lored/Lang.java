b6a901958ebf471bdaf75f03b50c58e1c1314804
Joel @ Laptop <joel@joelurw.in>
2022-10-23 01:56:16 +0100

    Changed pom.xml, Added lang enum and lang file. Updated Lored.java to add a language loader.

---
+++ src/in/joelurw/Lored/Lang.java
@@ -0,0 +1,69 @@
+package in.joelurw.Lored;
+
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public enum Lang {
+    DEFAULT_LINE("defaultline",""),
+    SUCCESS("success","Successfully applied the lore scroll."),
+    BROADCAST("broadcast","%s just applied a lore scroll!"),
+    STORE_URL("store-url",""),
+    STORE_URL_HOVER("store-url-hover-message","Click here to go to our store!"),
+    INAPPLICABLE("inapplicable ","This item already has a lore scroll applied!"),
+    OVERSTACKED("overstacked-item","Only one item can be lored!"),
+    UNAPPLIED_("unapplied","This item has not had a lore scroll applied!"),
+    ALREADY_LOCKED("already-locked","The lore scroll on this item has been locked!"),
+    LINE_LIMIT("line-limit","You have reached the line limit! Max, "),
+    EMPTY_INPUT("empty-input","Enter a lore line number and text."),
+    INCORRECT_LINE("incorrect-line","Incorrect lore line."),
+    CHAR_LIMIT("char-limit","Maximum Character Limit is "),
+    NO_TITLE("no-title","Enter a title."),
+    ADDED_LINE("added-line","Added line %s"),
+    CHANGED_LINE("changed-line","Changed lore line %s to %s"),
+    CLEARED("cleared","Successfully cleared lore lines."),
+    LOCKED("locked","Successfully locked the lore to your item!"),
+    TITLE_CHANGE("title-change","Successfully changed title to %s");
+
+    private String path;
+    private String def;
+    private static YamlConfiguration LANG;
+
+    /**
+     * Lang enum constructor.
+     * @param path The string path.
+     * @param start The default string.
+     */
+    Lang(String path, String start) {
+        this.path = path;
+        this.def = start;
+    }
+
+    /**
+     * Set the {@code YamlConfiguration} to use.
+     * @param config The config to set.
+     */
+    public static void setFile(YamlConfiguration config) {
+        LANG = config;
+    }
+
+    @Override
+    public String toString() {
+        return ChatColor.translateAlternateColorCodes('&', LANG.getString(this.path, def));
+    }
+
+    /**
+     * Get the default value of the path.
+     * @return The default value of the path.
+     */
+    public String getDefault() {
+        return this.def;
+    }
+
+    /**
+     * Get the path to the string.
+     * @return The path to the string.
+     */
+    public String getPath() {
+        return this.path;
+    }
+}
--- src/in/joelurw/Lored/Lored.java
+++ src/in/joelurw/Lored/Lored.java
@@ -3,8 +3,18 @@ package in.joelurw.Lored;
 import in.joelurw.Lored.commands.Lore;
 import in.joelurw.Lored.events.LoreScrollApplyEvent;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Lored extends JavaPlugin {
+
+    public static YamlConfiguration LANG;
+    public static File LANG_FILE;
 
     public static Lored getPlugin() {
         return plugin;
@@ -16,7 +26,7 @@ public class Lored extends JavaPlugin {
     public void onEnable(){
         plugin = this;
         ItemManagement.init();
-        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[Lored by JoelUrwin] : Enabled!");
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[Lored by Asynchronized] : Enabled!");
         getServer().getPluginManager().registerEvents(new LoreScrollApplyEvent(), this);
         getCommand("lore").setExecutor(new Lore());
 
@@ -25,5 +35,54 @@ public class Lored extends JavaPlugin {
     @Override
     public void onDisable(){}
 
+
+    /**
+     * Load the lang.yml file.
+     *
+     * @return The lang.yml config.
+     */
+
+    public FileConfiguration loadLang() {
+        File lang = new File(getDataFolder(), "lang.yml");
+        if (!lang.exists()) {
+            try {
+                getDataFolder().mkdir();
+                lang.createNewFile();
+                InputStream defConfigStream = this.getResource("lang.yml");
+                if (defConfigStream != null) {
+                    YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(lang);
+                    defConfig.save(lang);
+                    Lang.setFile(defConfig);
+                    return defConfig;
+                }
+            } catch(IOException e) {
+                e.printStackTrace(); // So they notice
+                this.setEnabled(false); // Without it loaded, we can't send them messages
+            }
+        }
+        YamlConfiguration conf = YamlConfiguration.loadConfiguration(lang);
+        for(Lang item:Lang.values()) {
+            if (conf.getString(item.getPath()) == null) {
+                conf.set(item.getPath(), item.getDefault());
+            }
+        }
+        Lang.setFile(conf);
+        Lored.LANG = conf;
+        Lored.LANG_FILE = lang;
+        try {
+            conf.save(getLangFile());
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return conf;
+    }
+
+    public File getLangFile() {
+        return LANG_FILE;
+    }
+
+    public YamlConfiguration getLang() {
+        return LANG;
+    }
 
 }
